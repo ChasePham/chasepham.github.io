f6decb8c691a08d2c7860bbfac2dc3deb581e635
ChasePham <chase9659@gmail.com>
2026-09-07 10:42:50 -0400

    outline website in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import SideBar from './components/SideBar.js'
+import About from './components/About.js'
+import Experience from './components/Experience.js'
+import Resume from './components/Resume.js'
+import Contact from './components/Contact.js'
 function App() {
   return (
     <div>
-      
+      <SideBar></SideBar>
+      <main>
+        <About></About>
+        <Experience></Experience>
+        <Reel></Reel>
+        <Resume></Resume>
+        <Contact></Contact>
+      </main>
     </div>
   );
 }
