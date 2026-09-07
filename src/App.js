5d3171022f8b6d5b49260206fc9b239ca7f1f9e2
ChasePham <chase9659@gmail.com>
2026-09-07 10:13:11 -0400

    initial branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-
 import './App.css';
-import NavBar from './components/Navbar.js';
-import Home from './components/Home.js';
-import About from './components/aboutMeSection/About.js'
-import Experience from './components/experienceSection/Experience.js'
-import Resume from './components/Resume.js';
+
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
-      <Home></Home>
-      <About></About>
-      <Experience></Experience>
-      <Resume></Resume>
+      
     </div>
   );
 }
- 
+
 export default App;
